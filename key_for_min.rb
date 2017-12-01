7eaf82b66dd168c1e96c76bd3f6da10019b25631
Mark Bello <mcbello51286@gmail.com>
2017-12-01 15:31:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,14 +12,16 @@ def key_for_min_value(name_hash)
   min_value = values[0]
   values.each do |value|
     if value < min_value
-      value = min_value
+      min_value = value
     end
   end
 
   #return the matching key
+  min_key = nil
   name_hash.each do |key, value|
     if value = min_value
-      return key
+      min_key = key
     end
   end
+  min_key
 end
